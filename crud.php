c89e12692956b0791d240fb0a6f301157ca39c72
givano750 <givano550@gmail.com>
2025-04-18 18:38:41 +0800

    Fixed crud, database procedures

--- crud.php
+++ crud.php
@@ -9,49 +9,47 @@ class Crud {
         $this->conn = $db->getConnection();
     }
 
-    // Create a new patient
+    // Create a new patient using the stored procedure
     public function create($first_name, $middle_name, $last_name, $age, $sex, $contact, $address, $blood, $date_of_birth, $height, $weight, $photo) {
-        $stmt = $this->conn->prepare("INSERT INTO patients (first_name, middle_name, last_name, age, sex, contact_number, address, blood_type, date_of_birth, height, weight, photo) 
-                                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt = $this->conn->prepare("CALL AddPatient(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->execute([$first_name, $middle_name, $last_name, $age, $sex, $contact, $address, $blood, $date_of_birth, $height, $weight, $photo]);
     }
-    
 
-    // Read a patient by ID
+    // Read a patient by ID using the stored procedure
     public function read($id) {
-        $stmt = $this->conn->prepare("SELECT * FROM patients WHERE patient_id = ?");
+        $stmt = $this->conn->prepare("CALL GetPatient(?)");
         $stmt->execute([$id]);
         return $stmt->fetch();
     }
 
-    // Update a patient's details (Modified to accept all 12 fields)
+    // Update a patient's details using the stored procedure
     public function update($id, $first_name, $middle_name, $last_name, $age, $sex, $contact, $address, $blood, $height, $weight, $date_of_birth) {
-        $stmt = $this->conn->prepare("UPDATE patients SET 
-                                      first_name = ?, 
-                                      middle_name = ?, 
-                                      last_name = ?, 
-                                      age = ?, 
-                                      sex = ?, 
-                                      contact_number = ?, 
-                                      address = ?, 
-                                      blood_type = ?, 
-                                      height = ?, 
-                                      weight = ?, 
-                                      date_of_birth = ?, 
-                                      updated_at = CURRENT_TIMESTAMP 
-                                      WHERE patient_id = ?");
-        return $stmt->execute([$first_name, $middle_name, $last_name, $age, $sex, $contact, $address, $blood, $height, $weight, $date_of_birth, $id]);
+        $stmt = $this->conn->prepare("CALL UpdatePatient(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        return $stmt->execute([
+            $id,
+            $first_name,
+            $middle_name,
+            $last_name,
+            $age,
+            $sex,
+            $contact,
+            $address,
+            $blood,
+            $height,
+            $weight,
+            $date_of_birth
+        ]);
     }
 
-    // Delete a patient by ID
+    // Delete a patient by ID using the stored procedure
     public function delete($id) {
-        $stmt = $this->conn->prepare("DELETE FROM patients WHERE patient_id = ?");
+        $stmt = $this->conn->prepare("CALL RemovePatient(?)");
         return $stmt->execute([$id]);
     }
 
-    // Get all patients
+    // Get all patients using the stored procedure
     public function getAll() {
-        $stmt = $this->conn->prepare("SELECT * FROM patients");
+        $stmt = $this->conn->prepare("CALL GetAllPatients()");
         $stmt->execute();
         return $stmt->fetchAll();
     }
