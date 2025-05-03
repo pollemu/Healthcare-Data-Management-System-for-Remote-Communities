7ed10a1daa78172af592c90bf07112cf990b6ca2
JanaMatanguihan <janamatanguihan.edu@gmail.com>
2025-05-03 23:51:40 +0800

    test

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = 'localhost';
-    private $db_name = 'healthcare_data-2';
+    private $db_name = 'healthcare_data';
     private $username = 'root';
     private $password = '';
     private $conn;
