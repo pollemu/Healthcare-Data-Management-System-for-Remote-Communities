509e3b3bd5b5884a2b9bea7fa22bff79b7b16d5f
JanaMatanguihan <janamatanguihan.edu@gmail.com>
2025-05-09 22:53:14 +0800

    edited dashboard and dashboard panel

--- Inventory/del_medicine.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'CrudInventory.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
-    $inventory = new CrudInventory();
-    $inventory->deleteMedicine($_POST['id']);
-}
-header("Location: ../Nav/dashboard_panel.php?page=inventory");
-exit;
